6ab9fc96e541b7f54fa59413c3b603ff1eeeb28e
Cheikh <bayecheikhgueye2@gmail.com>
2023-01-13 16:59:11 +0000

    Update

---
+++ pages/marchepublics/addMarchepublic.vue
@@ -0,0 +1,70 @@
+M<template>
+  <div>
+  <div class="custom-container bg-title-grey">
+      <page-header :items="headerItems" class=""></page-header>  
+  </div> 
+  <div class="custom-container mt-5">
+    <v-card class="container pl-10 pt-10 pb-10 pr-10" flat>  
+      <form-add-marchepublic></form-add-marchepublic>
+    </v-card>
+  </div> 
+</div>
+</template>
+
+<script>
+import LeftMenu from '@/components/LeftMenu';
+import PageHeader from '@/components/PageHeader';
+import FormAddMarchepublic from '@/components/marchepublics/FormAddMarchepublic';
+  export default {
+    layout: "dashboard",
+    components: {
+      LeftMenu,
+      PageHeader,
+      FormAddMarchepublic
+    },
+    middleware: function ({redirect,$hasPermission}) {
+      if(!$hasPermission('gerer-parametres')){
+        return redirect('/')
+      }
+    },
+    data () {
+      return {
+        leftmenuItems: [
+          { text: 'Roles', icon: 'mdi-lock',link:'/roles',position:1  },
+          { text: 'marchepublics', icon: 'mdi-lock',link:'/marchepublics',position:2  }
+        ],
+        headerItems: [
+        {
+          text: 'Marche publics',
+          disabled: false,
+          to: '/marchepublics',
+          exact: true
+        },
+        {
+          text: 'Nouveau marché public',
+          disabled: true,
+          to: '/marchepublics/addMarchepublic',
+          exact: true
+        }
+        
+        ],
+        date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        date1: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        date2: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        date3: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
+        menu1: false,
+        menu2: false,
+        menu3: false,
+        modal: false,
+        marchepublics: ['Sperviseur national'],
+        localisations: ['Thiès','Dakar'],
+
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/marchepublics/detail/_id.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+  <div class="custom-container bg-title-grey">
+      <page-header :items="headerItems" class=""></page-header>  
+  </div> 
+  <div class="custom-container mt-5">
+    <v-card class="container pl-10 pt-10 pb-10 pr-10" flat>  
+     <v-row align="center"
+            justify="center">
+            <v-col cols="12" md="12" lg="12" sm="12">
+              <info-marchepublic></info-marchepublic>
+            </v-col>
+            <v-col cols="12" md="12" lg="12" sm="12">
+              <actions-marchepublic></actions-marchepublic>
+            </v-col>
+          </v-row>
+    </v-card>
+  </div> 
+</div>
+</template>
+
+<script>
+import LeftMenu from '@/components/LeftMenu';
+import PageHeader from '@/components/PageHeader';
+import InfoMarchepublic from '@/components/marchepublics/detail/InfoMarchepublic';
+import ActionsMarchepublic from '@/components/marchepublics/detail/ActionsMarchepublic';
+  export default {
+    layout: "dashboard",
+    components: {
+      LeftMenu,
+      PageHeader,
+      InfoMarchepublic,
+      ActionsMarchepublic
+    },
+    middleware: function ({redirect,$hasPermission}) {
+      if(!$hasPermission('gerer-parametres')){
+        return redirect('/')
+      }
+    },
+    data () {
+      return {
+        leftmenuItems: [
+          { text: 'Roles', icon: 'mdi-lock',link:'/roles',position:1  },
+          { text: 'marchepublics', icon: 'mdi-lock',link:'/marchepublics',position:2  }
+        ],
+        headerItems: [
+        {
+          text: 'Marchés publics',
+          disabled: false,
+          to: '/marchepublics',
+          exact: true
+        },
+        {
+          text: 'Detail marché public',
+          disabled: true,
+          to: '/marchepublics/94',
+          exact: true
+        }
+        
+        ]
+
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
